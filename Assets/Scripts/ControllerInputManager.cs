cd1f3c4e2c262ba59279ad8b2f99af4d40bb8c7f
J Robalino <J Robalino>
2017-04-18 00:00:25 -0500

    feat: Add teleportation functionality.

---
+++ Assets/Scripts/ControllerInputManager.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInputManager : MonoBehaviour {
+
+	public bool leftController;
+	
+	//public SteamVR_TrackedObject trackedObj;
+	public SteamVR_Controller.Device leftDevice;
+	//public SteamVR_Controller.Device rightDevice;
+	int leftIndex;
+
+
+	// Teleporter
+	public LineRenderer laser;
+	public GameObject teleportAimerObject;
+	private Vector3 teleportLocation;
+	public GameObject player;
+	public LayerMask laserMask;
+	public float yNudgeAmount = 2f; //specific to teleportAimerObject right
+	public float teleporterMaxHorizontal = 15.0f;
+	public float teleporterMaxVertical = 17.0f;
+	public float playerHeight = 2.0f;
+	private bool canTeleport = false;
+
+	// Dash
+	public bool useDash;
+	public float dashSpeed = 0.1f;
+	private bool isDashing;
+	private float lerpTime;
+	private Vector3 dashStartPosition;
+
+	// Walking
+	public bool allowWalking;
+	public Transform playerCam;
+	public float moveSpeed = 4f;
+	private Vector3 movementDirection;
+
+
+	// Use this for initialization
+	void Start () {
+		//trackedObj = GetComponent<SteamVR_TrackedObject>();
+		//int rightIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Rightmost);
+		leftIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		leftDevice = SteamVR_Controller.Input(leftIndex);
+		//rightDevice = SteamVR_Controller.Input(rightIndex);
+
+		if (allowWalking && leftDevice.GetPress(SteamVR_Controller.ButtonMask.Grip))
+		{
+			movementDirection = playerCam.transform.forward;
+			movementDirection = new Vector3(movementDirection.x, 0, movementDirection.z); // this assumes floor is always at y = 0
+			movementDirection = movementDirection * moveSpeed * Time.deltaTime;
+			player.transform.position += movementDirection;
+		}
+		
+		if (isDashing && useDash)
+		{
+			lerpTime += 1 * dashSpeed;
+			player.transform.position = Vector3.Lerp(dashStartPosition, teleportLocation + new Vector3(0, playerHeight, 0), lerpTime);
+
+			if (lerpTime >= 1)
+			{
+				isDashing = false;
+				lerpTime = 0;
+			}
+		}
+		
+		else
+		{
+			
+			if (leftDevice.GetPress(SteamVR_Controller.ButtonMask.Touchpad) && laser != null)
+			{
+				canTeleport = false;
+				//teleportAimerObject.GetComponent<Renderer>().material.color = Color.white;
+				laser.gameObject.SetActive(true);
+				teleportAimerObject.SetActive(true);
+
+				laser.SetPosition(0, gameObject.transform.position);
+				RaycastHit hit;
+				if (Physics.Raycast(transform.position, transform.forward, out hit, teleporterMaxHorizontal, laserMask))
+				{
+					canTeleport = true;
+					teleportLocation = hit.point;
+					laser.SetPosition(1, teleportLocation);
+					//aimer position
+					teleportAimerObject.transform.position = new Vector3(teleportLocation.x, teleportLocation.y + yNudgeAmount, teleportLocation.z);
+				}
+				else
+				{
+					canTeleport = false;
+					teleportLocation = new Vector3(transform.forward.x * teleporterMaxHorizontal + transform.position.x, transform.forward.y * teleporterMaxHorizontal + transform.position.y, transform.forward.z * teleporterMaxHorizontal + transform.position.z);
+					RaycastHit groundRay;
+					if (Physics.Raycast(teleportLocation, -Vector3.up, out groundRay, teleporterMaxVertical, laserMask))
+					{
+						canTeleport = true;
+						teleportLocation = new Vector3(transform.forward.x * teleporterMaxHorizontal + transform.position.x, groundRay.point.y, transform.forward.z * teleporterMaxHorizontal + transform.position.z);
+					}
+					laser.SetPosition(1, transform.forward * teleporterMaxHorizontal + transform.position);
+					//aimer
+					teleportAimerObject.transform.position = teleportLocation + new Vector3(0, yNudgeAmount, 0);
+
+				}
+			}
+		}
+		if (leftDevice.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad) && laser != null)
+		{
+			laser.gameObject.SetActive(false);
+			teleportAimerObject.SetActive(false);
+
+			if (canTeleport)
+			{
+				if(useDash)
+				{
+					dashStartPosition = player.transform.position;
+					isDashing = true;
+				}
+				else player.transform.position = new Vector3(teleportLocation.x, teleportLocation.y + playerHeight, teleportLocation.z);
+				
+			}
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/ControllerInputManagerEditor.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+[CustomEditor(typeof(ControllerInputManager))]
+public class ControllerInputEditor : Editor
+{
+
+	override public void OnInspectorGUI()
+	{
+		var ControllerInputManager = target as ControllerInputManager;
+
+		ControllerInputManager.leftController = EditorGUILayout.Toggle("Left Controller", ControllerInputManager.leftController);
+
+		using (var group = new EditorGUILayout.FadeGroupScope(Convert.ToSingle(ControllerInputManager.leftController)))
+		{
+			if (group.visible == true)
+			{
+				EditorGUI.indentLevel++;
+				EditorGUILayout.PrefixLabel("Teleport Aimer Object");
+				ControllerInputManager.teleportAimerObject = (GameObject)EditorGUILayout.ObjectField(ControllerInputManager.teleportAimerObject, typeof(GameObject), true);
+				EditorGUILayout.PrefixLabel("Laser");
+				ControllerInputManager.laser = (LineRenderer)EditorGUILayout.ObjectField(ControllerInputManager.laser, typeof(LineRenderer), true);
+				EditorGUILayout.PrefixLabel("Player");
+				ControllerInputManager.player = (GameObject)EditorGUILayout.ObjectField(ControllerInputManager.player, typeof(GameObject), true);
+				ControllerInputManager.laserMask = EditorTools.LayerMaskField("Laser Mask", ControllerInputManager.laserMask);
+				EditorGUILayout.PrefixLabel("yNudgeAmount");
+				ControllerInputManager.yNudgeAmount = EditorGUILayout.FloatField(ControllerInputManager.yNudgeAmount);
+				EditorGUILayout.PrefixLabel("Horizontal Range");
+				ControllerInputManager.teleporterMaxHorizontal = EditorGUILayout.FloatField(ControllerInputManager.teleporterMaxHorizontal);
+				EditorGUILayout.PrefixLabel("Vertical Range");
+				ControllerInputManager.teleporterMaxVertical = EditorGUILayout.FloatField(ControllerInputManager.teleporterMaxVertical);
+				EditorGUILayout.PrefixLabel("Player Height");
+				ControllerInputManager.playerHeight = EditorGUILayout.FloatField(ControllerInputManager.playerHeight);
+				EditorGUILayout.PrefixLabel("Allow Dash");
+				ControllerInputManager.useDash = EditorGUILayout.Toggle(ControllerInputManager.useDash);
+				EditorGUILayout.PrefixLabel("Dash Speed");
+				ControllerInputManager.dashSpeed = EditorGUILayout.FloatField(ControllerInputManager.dashSpeed);
+				EditorGUILayout.PrefixLabel("Allow Walking");
+				ControllerInputManager.allowWalking = EditorGUILayout.Toggle(ControllerInputManager.allowWalking);
+				EditorGUILayout.PrefixLabel("Player Cam");
+				ControllerInputManager.playerCam = (Transform)EditorGUILayout.ObjectField(ControllerInputManager.playerCam, typeof(Transform), true);
+				EditorGUILayout.PrefixLabel("Walk Speed");
+				ControllerInputManager.moveSpeed = EditorGUILayout.FloatField(ControllerInputManager.moveSpeed);
+				EditorGUI.indentLevel--;
+			}
+		}
+	}
+}
+
+
