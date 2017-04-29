d7e3ebf9b736a0b1f094198f8c3c0f64d5baa64f
J Robalino <J Robalino>
2017-04-28 19:25:58 -0500

    feat: Add colliders to the main static objects in scene.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	public AudioSource introClip;
+	
+	// Use this for initialization
+	void Start () {
+		introClip.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
